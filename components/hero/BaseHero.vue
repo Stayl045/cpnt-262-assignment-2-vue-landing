ca3866f59735077036014cca0d7994e873f83394
Stephan Taylor <stephantaylor21@hotmail.com>
2022-04-01 11:07:34 -0600

    added details to the components

---
+++ components/hero/BaseHero.vue
@@ -0,0 +1,24 @@
+<template>
+  <section
+    v-for="hero in hero"
+    :key="hero.id"
+    :style="{ 'background-image': `url(${hero.image.backgroundImage})` }"
+  >
+    <HeroTitle>{{ mainHero.title }}</HeroTitle>
+    <HeroDescription>{{ HeroDescription }}</HeroDescription>
+  </section>
+</template>
+
+<script setup>
+  import HeroTitle from "./HeroTitle.vue";
+
+  const hero = [
+    {
+      title: "This is the best",
+      description: "",
+      image: {
+        backgroundImage: "",
+      },
+    },
+  ];
+</script>
