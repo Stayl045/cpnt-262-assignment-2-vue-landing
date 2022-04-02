d4bc26f0a02b07afa59062c713ee6033d0711859
Stephan Taylor <stephantaylor21@hotmail.com>
2022-04-01 20:50:11 -0600

    completed footer section

--- components/footer/BaseFooter.vue
+++ components/footer/BaseFooter.vue
@@ -1,12 +1,19 @@
 <template>
-  <section class="bg-black h-96 mt-7 text-white">
+  <section
+    class="grid grid-cols-3 bg-black opacity-95 h-96 mt-7 text-white w-full"
+  >
     <FooterCopyright>
       <footer>
         <p>&copy; Stephan Taylor</p>
       </footer>
     </FooterCopyright>
-    <div>
-      <FooterSocialMedia />
+    <div class="grid col-start-5 pt-14 pr-12">
+      <FooterSocialMedia>
+        <a :href="'/'" class="hover:text-red-400 text-3xl">Social</a>
+        <a :href="'/'" class="hover:text-red-400">Instagram</a>
+        <a :href="'/'" class="hover:text-red-400">FaceBook</a>
+        <a :href="'/'" class="hover:text-red-400">YouTube</a>
+      </FooterSocialMedia>
     </div>
   </section>
 </template>
--- components/footer/FooterCopyright.vue
+++ components/footer/FooterCopyright.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="grid col-start-1 col-end-3 text-3xl font-extrabold pt-14 pl-9">
     <slot></slot>
   </div>
 </template>
